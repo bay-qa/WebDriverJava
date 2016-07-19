3669477dc995bc177d5047a5f484232ce54531e5
ab054 <alfamiliya@gmail.com>
2016-07-18 20:05:25 -0700

    test001 added

--- src/test/java/browserTests/Day2.java
+++ src/test/java/browserTests/Day2.java
@@ -1,6 +1,10 @@
 package browserTests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 /**
  * Created by abarabash on 7/18/16.
@@ -11,6 +15,30 @@ public class Day2 extends BaseTest{
     public void checkboxDiscoveryTest(){
 
         System.out.println("checkBoxTest started");
+
+        driver.get("http://the-internet.herokuapp.com/checkboxes");
+
+        List<WebElement> checkboxes = driver.findElements(By.cssSelector("input[type=\"checkbox\"]"));
+
+        System.out.println("using attribute for checkboxes");
+        //iterating through the checkboxes list
+        for (WebElement eachCheckbox : checkboxes){
+
+            System.out.println(String.valueOf(eachCheckbox.getAttribute("checked")));
+        }
+
+        System.out.println("using isSelected for checkboxes");
+        //iterating through the checkboxes list
+        for (WebElement eachCheckbox : checkboxes){
+
+            System.out.println(eachCheckbox.isSelected());
+        }
+
+
+
+
+
+
 
 
 
