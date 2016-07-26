08f58c98b8ba98cd5ff58b2cefa92482c7be481a
ab054 <alfamiliya@gmail.com>
2016-07-25 19:24:32 -0700

    test001

--- src/test/java/browserTests/Day4.java
+++ src/test/java/browserTests/Day4.java
@@ -1,7 +1,32 @@
 package browserTests;
 
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
 /**
  * Created by abarabash on 7/25/16.
  */
 public class Day4 extends BaseTest {
+
+    @Test
+    public void upLoadFile() {
+
+        String fileName = "some-file.txt";
+
+        File file = new File(fileName);
+
+        String absolutePath = file.getAbsolutePath();
+
+        driver.get("http://the-internet.herokuapp.com/upload");
+
+        driver.findElement(By.id("file-upload")).sendKeys(absolutePath);
+
+        driver.findElement(By.id(("file-submit"))).click();
+
+
+    }
+
+
 }
